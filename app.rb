330c2fe5b075d41482ae90478229334e3517f590
Sebastian <sebastian.290794@gmail.com>
2022-06-20 22:22:06 -0500

    feat: CRUD of API was created

--- app.rb
+++ app.rb
@@ -51,3 +51,68 @@ end
 get '/user-info/:name' do 
     "Hello #{params["name"]}!"
 end
+
+
+# CREATING A CRUD
+
+## Model/Dataset
+
+payloads = [{title: "First Post", body: "Content of first post"}]
+
+# Custom Method for Getting Request Body
+def getBody (req)
+
+    ## Rewind the body in case it has already been read
+    req.body.rewind
+    ## parse to body
+    return JSON.parse(req.body.read)
+end
+
+## Index route
+get '/payloads' do
+    # Return all the payloads as JSON
+    return payloads.to_json
+end
+
+## Show Route
+get '/payloads/:id' do
+    # Return a particualr post as json based on the id param from the url
+    # Params always come to a string so we convert to an integer
+    id = params["id"].to_i
+    return payloads[id].to_json
+end
+
+## Create Route
+post '/payloads' do
+    # Pass the request insto the custom getBody function
+    body = getBody(request)
+    # Create the new payload
+    new_post = { title: body["title"], body: body["body"]}
+    # Push the new payload into the array
+    payloads.push(new_post)
+    # Return the new payload
+    return new_post.to_json
+end
+
+## Update Route
+put '/payloads/:id' do
+    # Get the id from params
+    id = params["id"].to_i
+    # Get the request body
+    body = getBody(request)
+    # Update the item in question
+    payloads[id][:title] = body["title"]
+    payloads[id][:body] = body["body"]
+    # Return the updated post
+    return payloads[id].to_json
+end
+
+## Delete Route
+delete '/payloads/:id' do  
+    # Get the id from params
+    id = params["id"].to_i
+    # Delete the item
+    payloads = payloads.delete_at(id)
+    # Return the deleted item as json
+    return payloads.to_json
+end
